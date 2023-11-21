6c4bd50d1235a529ffddd3763c66a1d04dc240ac
Hugo Deleye <contact@hug0.xyz>
2023-11-22 00:14:48 +0100

    add responsive

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -18,16 +18,17 @@ export default function Home() {
           </video>
         </div>
         <div className="mid-card bg-glass">
-          <div className="grid grid-cols-2 gap-14">
+          <div className="flex flex-col sm:flex-row gap-14">
             <div>
-              <div className="card-name">Hugo<br/>Deleye</div>
-              <div className="card-title pt-6">AI student @ Toulouse, France</div>
+              <div className="card-name">Hugo</div>
+              <div className="card-name pt-1">Deleye</div>
+              <div className="card-title pt-4">AI student @ Toulouse, France</div>
             </div>
-            <div className="flex flex-col justify-between">
-              <Link href={"https://www.linkedin.com/in/hugo-deleye/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={linkedin_icon} alt="Linkedin icon"/>LinkedIn</div></div></Link>
-              <Link href={"https://www.github.com/hgdly/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={github_icon} alt="GitHub icon"/>GitHub</div></div></Link>
-              <Link href={"https://open.spotify.com/user/i5yqjtdeo8855bsjb4oubsqyz"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={spotify_icon} alt="Spotify icon"/>Spotify</div></div></Link>
-              <div className="flex flex-row justify-between">
+            <div className="flex flex-col justify-between gap-y-4 sm:gap-y-0 pt-4 sm:pt-0">
+              <Link href={"https://www.linkedin.com/in/hugo-deleye/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={linkedin_icon} alt="Linkedin icon" className='social_icon'/>LinkedIn</div></div></Link>
+              <Link href={"https://www.github.com/hgdly/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={github_icon} alt="GitHub icon" className='social_icon'/>GitHub</div></div></Link>
+              <Link href={"https://open.spotify.com/user/i5yqjtdeo8855bsjb4oubsqyz"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={spotify_icon} alt="Spotify icon" className='social_icon'/>Spotify</div></div></Link>
+              <div className="flex flex-row justify-between pt-10 sm:pt-0">
                 <Link href={"https://cv.hgdly.me"} target='_blank'><div className="social-button">CV</div></Link>
                 <Link href={"https://projects.hgdly.me"} target='_blank'><div className="social-button">Projects</div></Link>
                 <Link href={"mailto:hugo@hgdly.me"} target='_blank'><div className="social-button">Mail</div></Link>

1f4a6c1033990ae5546609ac0d4402b80a685fc4
Hugo Deleye <contact@hug0.xyz>
2024-02-14 14:45:41 +0100

    Add cv, change link and fix

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import linkedin_icon from "@/public/linkedin.svg"
 import github_icon from "@/public/github.svg"
-import spotify_icon from "@/public/spotify.svg"
+import deezer_icon from "@/public/deezer.svg"
 import Link from 'next/link'
 
 export default function Home() {
@@ -27,10 +27,10 @@ export default function Home() {
             <div className="flex flex-col justify-between gap-y-4 sm:gap-y-0 pt-4 sm:pt-0">
               <Link href={"https://www.linkedin.com/in/hugo-deleye/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={linkedin_icon} alt="Linkedin icon" className='social_icon'/>LinkedIn</div></div></Link>
               <Link href={"https://www.github.com/hgdly/"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={github_icon} alt="GitHub icon" className='social_icon'/>GitHub</div></div></Link>
-              <Link href={"https://open.spotify.com/user/i5yqjtdeo8855bsjb4oubsqyz"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={spotify_icon} alt="Spotify icon" className='social_icon'/>Spotify</div></div></Link>
-              <div className="flex flex-row justify-between pt-10 sm:pt-0">
-                <Link href={"https://cv.hgdly.me"} target='_blank'><div className="social-button">CV</div></Link>
-                <Link href={"https://projects.hgdly.me"} target='_blank'><div className="social-button">Projects</div></Link>
+              <Link href={"https://www.deezer.com/fr/profile/867246701"} target='_blank'><div className="social-button"><div className="flex gap-x-4"><Image src={deezer_icon} alt="Deezer icon" className='social_icon'/>Deezer</div></div></Link>
+              <div className="flex flex-row justify-between pt-10 sm:pt-0 px-16 sm:px-0 sm:gap-x-6">
+                <Link href={"./cv"} target='_blank'><div className="social-button">CV</div></Link>
+                {/* <Link href={"https://projects.hgdly.me"} target='_blank'><div className="social-button">Projects</div></Link> */}
                 <Link href={"mailto:hugo@hgdly.me"} target='_blank'><div className="social-button">Mail</div></Link>
               </div>
             </div>
